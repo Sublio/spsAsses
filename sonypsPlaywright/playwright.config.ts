b7657c79ce7096186b43fdfc1ad4d6a6b67b1564
Denis Mordvinov <sublio1260@gmail.com>
2024-02-04 16:38:20 +0000

    Test check cell auto selected after time constant

--- sonypsPlaywright/playwright.config.ts
+++ sonypsPlaywright/playwright.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
-  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 1 : undefined,
@@ -24,27 +14,6 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        viewport: { width: 1920, height: 1080 },
-        channel: 'chrome',
-        contextOptions: {
-          javaScriptEnabled: true,
-        },
-      },
-    },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-        viewport: { width: 1920, height: 1080 },
-        contextOptions: {
-          javaScriptEnabled: true,
-        },
-      },
-    },
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
         viewport: { width: 1920, height: 1080 },
         contextOptions: {
           javaScriptEnabled: true,
--- sonypsPlaywright/tests/sonytests.spec.ts
+++ sonypsPlaywright/tests/sonytests.spec.ts
@@ -4,6 +4,8 @@ import { CarouselPage } from './carouselPage';
 let page: Page;
 let carouselPage: CarouselPage;
 
+let itemsInCarouselConstant = 7;
+
 test.beforeEach(async ({ browser }) => {
   const context = await browser.newContext();
   page = await context.newPage();
@@ -18,7 +20,7 @@ test.afterEach(async () => {
 test('Test can select each item in carousel', async () => {
   const itemCount = await carouselPage.getCarouselItemCount();
 
-  expect(itemCount).toBe(7);
+  expect(itemCount).toBe(itemsInCarouselConstant);
 
   for (let i = 0; i < itemCount; i++) {
     await carouselPage.clickCarouselItem(i);
@@ -27,3 +29,36 @@ test('Test can select each item in carousel', async () => {
     expect(isSelected).toBe(true);
   }
 });
+
+test('Carousel switch cell selection automatically after time constant', async ({ page }) => {
+  test.setTimeout(120000);
+  const itemCount = await carouselPage.getCarouselItemCount();
+  const timeConstant = 10000; // 10 seconds in milliseconds
+  const pollingInterval = 1000; // Poll every 1 second
+  const maxAttempts = timeConstant / pollingInterval; // Maximum attempts based on timeConstant
+
+  expect(itemCount).toBe(itemsInCarouselConstant);
+
+  for (let i = 0; i < itemCount; i++) {
+    let isSelected = false;
+    let attempts = 0;
+
+    while (!isSelected && attempts < maxAttempts) {
+      // Wait for the specified polling interval
+      await page.waitForTimeout(pollingInterval);
+      
+      // Check if the element has the "is-selected" class
+      isSelected = await carouselPage.isCarouselCellSelected(i);
+      attempts++;
+    }
+
+    // Cell should eventually become selected
+    expect(isSelected).toBe(true);
+
+    // Log what's going on with current cell
+    console.log(`Cell ${i} is selected.`);
+  }
+});
+
+
+
